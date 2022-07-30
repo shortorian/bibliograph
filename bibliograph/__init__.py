ca76be7408ff9b6bf71e09c2047a37a1ee698d44
devin <short.devin@gmail.com>
2022-07-30 11:51:26 -0700

    import alias_generators module

--- bibliograph/__init__.py
+++ bibliograph/__init__.py
@@ -2,4 +2,5 @@ from bibliograph.TextNet import TextNet
 from bibliograph.core import slurp_bibtex
 from bibliograph.core import slurp_shorthand
 from bibliograph.core import textnet_from_parsed_shorthand
+import bibliograph.alias_generators as alias_generators
 import bibliograph.util as util
