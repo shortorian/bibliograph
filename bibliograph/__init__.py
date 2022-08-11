52210f40fc964e9101b3db8c345bcd920e0a5ab7
devin <short.devin@gmail.com>
2022-08-11 12:20:18 -0700

    make module imports explicit

--- bibliograph/__init__.py
+++ bibliograph/__init__.py
@@ -4,7 +4,7 @@ from bibliograph.TextNet import TextNet
 from bibliograph.core import slurp_bibtex
 from bibliograph.core import slurp_shorthand
 from bibliograph.core import textnet_from_parsed_shorthand
-import bibliograph.alias_generators
-import bibliograph.entry_parsing
-import bibliograph.syntax_parsing
-import bibliograph.util
+import bibliograph.alias_generators as alias_generators
+import bibliograph.entry_parsing as entry_parsing
+import bibliograph.syntax_parsing as syntax_parsing
+import bibliograph.util as util
