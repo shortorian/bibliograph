c4671b5ecf25ee3303430e7ec206115d936b6925
devin <short.devin@gmail.com>
2022-10-25 11:42:42 -0700

    release 0.1.0 to pypi, updating requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='bibliograph',
-    version='0.0.1',
+    version='0.1.0',
     author='Devin Short',
     author_email='short.devin@gmail.com',
     packages=['bibliograph'],
@@ -18,17 +18,25 @@ setuptools.setup(
     license='MIT',
     python_requires='>=3',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English'
     ],
     install_requires=[
-        'bibtexparser>=1.3.0',
-        'numpy>=1.23.1',
-        'pandas>=1.4.3',
+        'attrs>=22.1.0',
+        'bibtexparser>=1.4.0',
+        'colorama>=0.4.6',
+        'exceptiongroup>=1.0.0rc9',
+        'iniconfig>=1.1.1',
+        'numpy>=1.23.4',
+        'packaging>=21.3',
+        'pandas>=1.5.1',
+        'pluggy>=1.0.0',
         'pyparsing>=3.0.9',
+        'pytest>=7.2.0',
         'python-dateutil>=2.8.2',
-        'pytz>=2022.1',
-        'six>=1.16.0'
+        'pytz>=2022.5',
+        'six>=1.16.0',
+        'tomli>=2.0.1'
     ]
 )
